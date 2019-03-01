16d36ba6288e08142c4b57c4724d5829499b8d53
Hexilee <hexileee@gmail.com>
2019-03-01 19:48:08 +0800

    add scores::SCORES_PAGE

--- src/tests/pages/mod.rs
+++ src/tests/pages/mod.rs
@@ -3,8 +3,11 @@ mod exams;
 mod login;
 mod major_scores;
 mod object_moved;
+mod scores;
+
 pub use self::courses::COURSES_PAGE;
 pub use self::exams::EXAMS_PAGE;
 pub use self::login::LOGIN_PAGE;
 pub use self::major_scores::MAJOR_SCORES_PAGE;
 pub use self::object_moved::OBJECT_MOVED_PAGE;
+pub use self::scores::SCORES_PAGE;
