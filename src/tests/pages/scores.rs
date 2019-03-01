d591e9c4da13e0b32be5568e297c4dc03cdb5d8b
Hexilee <hexileee@gmail.com>
2019-03-01 19:48:13 +0800

    add scores::SCORES_PAGE

---
+++ src/tests/pages/scores.rs
@@ -0,0 +1,174 @@
+pub const SCORES_PAGE: &str = r##"
+<!doctype HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+  <HEAD>
+		<title>浙江大学现代教务管理系统</title>
+		<META HTTP-EQUIV="Content-Type" content="text/html; charset=gb2312">
+		<META content="Microsoft Visual Studio .NET 7.0" name="GENERATOR">
+		<META content="Visual Basic 7.0" name="CODE_LANGUAGE">
+		<META content="JavaScript" name="vs_defaultClientScript">
+		<META content="http://schemas.microsoft.com/intellisense/ie5" name="vs_targetSchema">
+		<LINK rev="stylesheet" media="all" href="style/base.css" type="text/css" rel="stylesheet">
+		<LINK rev="stylesheet" media="all" href="style/form.css" type="text/css" rel="stylesheet">
+		<LINK rev="stylesheet" media="all" href="style/module.css" type="text/css" rel="stylesheet">
+		<SCRIPT language="javascript">
+            function window.onbeforeprint(){
+                document.all.tabHidden.style.display = "none"
+            }
+            function window.onafterprint(){
+                document.all.tabHidden.style.display = "block"
+            }
+            function click() {
+                if (event.button==2) {  //改成button==2为禁止右键
+                    alert('对不起,禁止使用此功能.')
+                }
+            }
+            document.onmousedown=click
+		</SCRIPT>
+</HEAD>
+	<body>
+		<form name="Form1" method="post" action="xscj.aspx?xh=" id="Form1">
+<input type="hidden" name="__VIEWSTATE" value="dDw0NzAzMzE4ODg7dDw7bDxpPDE+Oz47bDx0PDtsPGk8Mj47aTw1PjtpPDI1PjtpPDI3PjtpPDI5PjtpPDMxPjtpPDMzPjtpPDM1PjtpPDM3PjtpPDM5PjtpPDQxPjtpPDQzPjtpPDQ1PjtpPDQ3PjtpPDQ4Pjs+O2w8dDx0PDt0PGk8MjA+O0A8XGU7MjAwMS0yMDAyOzIwMDItMjAwMzsyMDAzLTIwMDQ7MjAwNC0yMDA1OzIwMDUtMjAwNjsyMDA2LTIwMDc7MjAwNy0yMDA4OzIwMDgtMjAwOTsyMDA5LTIwMTA7MjAxMC0yMDExOzIwMTEtMjAxMjsyMDEyLTIwMTM7MjAxMy0yMDE0OzIwMTQtMjAxNTsyMDE1LTIwMTY7MjAxNi0yMDE3OzIwMTctMjAxODsyMDE4LTIwMTk7MjAxOS0yMDIwOz47QDxcZTsyMDAxLTIwMDI7MjAwMi0yMDAzOzIwMDMtMjAwNDsyMDA0LTIwMDU7MjAwNS0yMDA2OzIwMDYtMjAwNzsyMDA3LTIwMDg7MjAwOC0yMDA5OzIwMDktMjAxMDsyMDEwLTIwMTE7MjAxMS0yMDEyOzIwMTItMjAxMzsyMDEzLTIwMTQ7MjAxNC0yMDE1OzIwMTUtMjAxNjsyMDE2LTIwMTc7MjAxNy0yMDE4OzIwMTgtMjAxOTsyMDE5LTIwMjA7Pj47Pjs7Pjt0PHQ8cDxwPGw8RGF0YVRleHRGaWVsZDtEYXRhVmFsdWVGaWVsZDs+O2w8eHhxO3hxMTs+Pjs+O3Q8aTw4PjtAPFxlO+aYpTvlpI8755+tO+enizvlhqw755+tO+aakTs+O0A8XGU7MnzmmKU7MnzlpI87Mnznn607MXznp4s7MXzlhqw7MXznn607MXzmmpE7Pj47Pjs7Pjt0PHA8O3A8bDxvbmNsaWNrOz47bDx3aW5kb3cucHJpbnQoKVw7Oz4+Pjs7Pjt0PHA8O3A8bDxvbmNsaWNrOz47bDx3aW5kb3cuY2xvc2UoKVw7Oz4+Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDIwMTYtMjAxN+WtpuW5tOWtpuS5oOaIkOe7qTs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w85a2m5Y+377yaMzE2MDEwMTAzNDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w85aeT5ZCN77ya5p2O5pmo5pumOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDzlrabpmaLvvJrmtbfmtIvlrabpmaI7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPOexuyjkuJPkuJop77ya5rW35rSL5bel56iL5LiO5oqA5pyvOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDzooYzmlL/nj63vvJrmtbfmtIvlt6XnqIvkuI7mioDmnK8xNjAyOz4+Oz47Oz47dDxAMDxwPHA8bDxQYWdlQ291bnQ7XyFJdGVtQ291bnQ7XyFEYXRhU291cmNlSXRlbUNvdW50O0RhdGFLZXlzOz47bDxpPDE+O2k8MjM+O2k8MjM+O2w8Pjs+Pjs+Ozs7Ozs7Ozs7Oz47bDxpPDA+Oz47bDx0PDtsPGk8MT47aTwyPjtpPDM+O2k8ND47aTw1PjtpPDY+O2k8Nz47aTw4PjtpPDk+O2k8MTA+O2k8MTE+O2k8MTI+O2k8MTM+O2k8MTQ+O2k8MTU+O2k8MTY+O2k8MTc+O2k8MTg+O2k8MTk+O2k8MjA+O2k8MjE+O2k8MjI+O2k8MjM+Oz47bDx0PDtsPGk8MD47aTwxPjtpPDI+O2k8Mz47aTw0PjtpPDU+Oz47bDx0PHA8cDxsPFRleHQ7PjtsPCgyMDE2LTIwMTctMSktMDIxRTAwMTAtMDA5MjQ2Ni01Oz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDzmgJ3mg7PpgZPlvrfkv67lhbvkuI7ms5Xlvovln7rnoYA7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDgzOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwyLjU7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDMuOTA7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPCZuYnNwXDs7Pj47Pjs7Pjs+Pjt0PDtsPGk8MD47aTwxPjtpPDI+O2k8Mz47aTw0PjtpPDU+Oz47bDx0PHA8cDxsPFRleHQ7PjtsPCgyMDE2LTIwMTctMSktMDIxRTAwMjAtMDAxNTE0OC0yOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDzkuK3lm73ov5HnjrDku6Plj7LnurLopoE7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDg1Oz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwyLjU7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDMuOTA7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPCZuYnNwXDs7Pj47Pjs7Pjs+Pjt0PDtsPGk8MD47aTwxPjtpPDI+O2k8Mz47aTw0PjtpPDU+Oz47bDx0PHA8cDxsPFRleHQ7PjtsPCgyMDE2LTIwMTctMSktMDIxTDAwMjAtMDA5MTEzNS0xOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDzms5Xlrabln7rnoYA7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDg5Oz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwxLjU7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDQuNTA7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPCZuYnNwXDs7Pj47Pjs7Pjs+Pjt0PDtsPGk8MD47aTwxPjtpPDI+O2k8Mz47aTw0PjtpPDU+Oz47bDx0PHA8cDxsPFRleHQ7PjtsPCgyMDE2LTIwMTctMSktMDUxRjAwMjAtMDA5NzM5MS0yOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDzlpKflraboi7Hor63ihaI7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDgwOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwzLjA7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDMuNjA7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPCZuYnNwXDs7Pj47Pjs7Pjs+Pjt0PDtsPGk8MD47aTwxPjtpPDI+O2k8Mz47aTw0PjtpPDU+Oz47bDx0PHA8cDxsPFRleHQ7PjtsPCgyMDE2LTIwMTctMSktMDgxQzAxMzAtMDA4ODA5NS0yOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDzlt6XnqIvlm77lraY7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDg0Oz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwyLjU7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDMuOTA7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPCZuYnNwXDs7Pj47Pjs7Pjs+Pjt0PDtsPGk8MD47aTwxPjtpPDI+O2k8Mz47aTw0PjtpPDU+Oz47bDx0PHA8cDxsPFRleHQ7PjtsPCgyMDE2LTIwMTctMSktMTAxTTAwMTAtMDA5NzAyMC0yOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDznlLXlt6XnlLXlrZDlt6XnqIvorq3nu4M7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDg0Oz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwxLjU7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDMuOTA7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPCZuYnNwXDs7Pj47Pjs7Pjs+Pjt0PDtsPGk8MD47aTwxPjtpPDI+O2k8Mz47aTw0PjtpPDU+Oz47bDx0PHA8cDxsPFRleHQ7PjtsPCgyMDE2LTIwMTctMSktMTAxTTAwMzAtMDA5MjQyMS0xOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDzmnLrnlLXkuIDkvZPljJbln7rnoYA7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDkwOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwxLjU7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDQuNTA7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPCZuYnNwXDs7Pj47Pjs7Pjs+Pjt0PDtsPGk8MD47aTwxPjtpPDI+O2k8Mz47aTw0PjtpPDU+Oz47bDx0PHA8cDxsPFRleHQ7PjtsPCgyMDE2LTIwMTctMSktMjExRzAyMzAtMDA5ODI0Ny0yOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDzorqHnrpfmnLrnp5Hlrabln7rnoYA7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDg1Oz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwyLjA7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDMuOTA7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPCZuYnNwXDs7Pj47Pjs7Pjs+Pjt0PDtsPGk8MD47aTwxPjtpPDI+O2k8Mz47aTw0PjtpPDU+Oz47bDx0PHA8cDxsPFRleHQ7PjtsPCgyMDE2LTIwMTctMSktNDAxMDA4MDAtMDA3OTIyNy0yOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDzmuLjms7Mo5Yid57qn54+t77yJOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDw4MDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8MS4wOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwzLjYwOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwmbmJzcFw7Oz4+Oz47Oz47Pj47dDw7bDxpPDA+O2k8MT47aTwyPjtpPDM+O2k8ND47aTw1Pjs+O2w8dDxwPHA8bDxUZXh0Oz47bDwoMjAxNi0yMDE3LTEpLTgyMVQwMDEwLTAwOTQzMTEtMTs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w85b6u56ev5YiG77yI55Sy77yJ4oWgOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDw4MTs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8NC41Oz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwzLjYwOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwmbmJzcFw7Oz4+Oz47Oz47Pj47dDw7bDxpPDA+O2k8MT47aTwyPjtpPDM+O2k8ND47aTw1Pjs+O2w8dDxwPHA8bDxUZXh0Oz47bDwoMjAxNi0yMDE3LTEpLTgyMVQwMDUwLTAwOTI0NzgtMTs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w857q/5oCn5Luj5pWw77yI55Sy77yJOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDw3Mjs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8Mi41Oz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwyLjcwOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwmbmJzcFw7Oz4+Oz47Oz47Pj47dDw7bDxpPDA+O2k8MT47aTwyPjtpPDM+O2k8ND47aTw1Pjs+O2w8dDxwPHA8bDxUZXh0Oz47bDwoMjAxNi0yMDE3LTIpLTAzMTEwMDIxLTAwMTY1MjMtMTs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w85Yab6K6tOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDw4MTs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8Mi4wOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwzLjYwOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwmbmJzcFw7Oz4+Oz47Oz47Pj47dDw7bDxpPDA+O2k8MT47aTwyPjtpPDM+O2k8ND47aTw1Pjs+O2w8dDxwPHA8bDxUZXh0Oz47bDwoMjAxNi0yMDE3LTIpLTAzMUUwMDEwLTAwODkyNTctMzs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w85Yab5LqL55CG6K66Oz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDw3Nzs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8MS41Oz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwzLjMwOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwmbmJzcFw7Oz4+Oz47Oz47Pj47dDw7bDxpPDA+O2k8MT47aTwyPjtpPDM+O2k8ND47aTw1Pjs+O2w8dDxwPHA8bDxUZXh0Oz47bDwoMjAxNi0yMDE3LTIpLTA1MUYwMDMwLTAwOTczOTEtMTs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w85aSn5a2m6Iux6K+t4oWjOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDw3Njs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8My4wOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwzOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwmbmJzcFw7Oz4+Oz47Oz47Pj47dDw7bDxpPDA+O2k8MT47aTwyPjtpPDM+O2k8ND47aTw1Pjs+O2w8dDxwPHA8bDxUZXh0Oz47bDwoMjAxNi0yMDE3LTIpLTA2MUIwMDEwLTAwMTIxMjUtMTs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w85bi45b6u5YiG5pa556iLOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDw3Nzs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8MS4wOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwzLjMwOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwmbmJzcFw7Oz4+Oz47Oz47Pj47dDw7bDxpPDA+O2k8MT47aTwyPjtpPDM+O2k8ND47aTw1Pjs+O2w8dDxwPHA8bDxUZXh0Oz47bDwoMjAxNi0yMDE3LTIpLTA4MUMwMjUxLTAwODgxMDMtMjs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w85bel56iL6K6t57uDOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDw4Mzs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8MS41Oz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwzLjkwOz4+Oz47Oz47dDxwPHA8bDxUZXh0Oz47bDwmbmJzcFw7Oz4+Oz47Oz47Pj47dDw7bDxpPDA+O2k8MT47aTwyPjtpPDM+O2k8ND47aTw1Pjs+O2w8dDxwPHA8bDxUZXh0Oz47bDwoMjAxNi0yMDE3LTIpLTIxMUcwMjEwLTAwOTgyNTMtMTs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8Q+eoi+W6j+iuvuiuoTs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8ODU7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDMuMDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8My45MDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8Jm5ic3BcOzs+Pjs+Ozs+Oz4+O3Q8O2w8aTwwPjtpPDE+O2k8Mj47aTwzPjtpPDQ+O2k8NT47PjtsPHQ8cDxwPGw8VGV4dDs+O2w8KDIwMTYtMjAxNy0yKS0yNDFTMDEwMC0wMDA0MTQ3LTE7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPOS4reWbveWcn+WcsOWItuW6pumXrumimOamguiuujs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8Qys7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDMuMDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8Mi44MDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8Jm5ic3BcOzs+Pjs+Ozs+Oz4+O3Q8O2w8aTwwPjtpPDE+O2k8Mj47aTwzPjtpPDQ+O2k8NT47PjtsPHQ8cDxwPGw8VGV4dDs+O2w8KDIwMTYtMjAxNy0yKS0zNzFFMDAxMC0wMDA2MDg0LTI7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPOW9ouWKv+S4juaUv+etluKFoDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8Njk7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDEuMDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8Mi40MDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8Jm5ic3BcOzs+Pjs+Ozs+Oz4+O3Q8O2w8aTwwPjtpPDE+O2k8Mj47aTwzPjtpPDQ+O2k8NT47PjtsPHQ8cDxwPGw8VGV4dDs+O2w8KDIwMTYtMjAxNy0yKS00MDEwMDgwMS0wMDEzMDIzLTE7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPOa4uOazs++8iOS4ree6p+ePre+8iTs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8ODg7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDEuMDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8NC4yMDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8Jm5ic3BcOzs+Pjs+Ozs+Oz4+O3Q8O2w8aTwwPjtpPDE+O2k8Mj47aTwzPjtpPDQ+O2k8NT47PjtsPHQ8cDxwPGw8VGV4dDs+O2w8KDIwMTYtMjAxNy0yKS02OTFCMDAxMC0wMDEwMDM1LTE7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPOa1t+a0i+amguiuujs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8NzM7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDMuMDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8Mi43MDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8Jm5ic3BcOzs+Pjs+Ozs+Oz4+O3Q8O2w8aTwwPjtpPDE+O2k8Mj47aTwzPjtpPDQ+O2k8NT47PjtsPHQ8cDxwPGw8VGV4dDs+O2w8KDIwMTYtMjAxNy0yKS03NjFUMDAxMC0wMDk0MTY5LTI7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPOWkp+WtpueJqeeQhu+8iOeUsu+8ieKFoDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8NTY7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDQuMDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8MDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8Jm5ic3BcOzs+Pjs+Ozs+Oz4+O3Q8O2w8aTwwPjtpPDE+O2k8Mj47aTwzPjtpPDQ+O2k8NT47PjtsPHQ8cDxwPGw8VGV4dDs+O2w8KDIwMTYtMjAxNy0yKS04MjFUMDAyMC0wMDg3MDM5LTE7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPOW+ruenr+WIhu+8iOeUsu+8ieKFoTs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8NTI7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDMuNTs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8MDs+Pjs+Ozs+O3Q8cDxwPGw8VGV4dDs+O2w8Jm5ic3BcOzs+Pjs+Ozs+Oz4+Oz4+Oz4+O3Q8QDA8Ozs7Ozs7Ozs7Oz47Oz47dDxAMDw7Ozs7Ozs7Ozs7Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPFpKRFg7Pj47Pjs7Pjt0PHA8cDxsPEltYWdlVXJsOz47bDwuL3RwbWwvOTcwNTI3LmpwZzs+Pjs+Ozs+Oz4+Oz4+Oz6nXwJ0NHccGU/bgidEmngSVD21sg==" />
+
+			<div class="indextop"></div>
+			<div class="indexmid">
+				<div class="toplogo">
+					<h6></h6>
+					<!-- <span><input type="text" value="输入帮助内容"> <a href="#"></a></span> -->
+				</div>
+				<div class="daohang">成绩查询</div>
+				<div class="searchbar"></div>
+				<div class="mainframe">
+					<TABLE id="tabHidden" cellSpacing="0" cellPadding="3">
+						<TR>
+							<TD nowrap><span id="Label1">学年：</span><select name="ddlXN" id="ddlXN">
+	<option value=""></option>
+	<option value="2001-2002">2001-2002</option>
+	<option value="2002-2003">2002-2003</option>
+	<option value="2003-2004">2003-2004</option>
+	<option value="2004-2005">2004-2005</option>
+	<option value="2005-2006">2005-2006</option>
+	<option value="2006-2007">2006-2007</option>
+	<option value="2007-2008">2007-2008</option>
+	<option value="2008-2009">2008-2009</option>
+	<option value="2009-2010">2009-2010</option>
+	<option value="2010-2011">2010-2011</option>
+	<option value="2011-2012">2011-2012</option>
+	<option value="2012-2013">2012-2013</option>
+	<option value="2013-2014">2013-2014</option>
+	<option value="2014-2015">2014-2015</option>
+	<option value="2015-2016">2015-2016</option>
+	<option selected="selected" value="2016-2017">2016-2017</option>
+	<option value="2017-2018">2017-2018</option>
+	<option value="2018-2019">2018-2019</option>
+	<option value="2019-2020">2019-2020</option>
+
+</select>
+								<span id="Label2">学期：</span><select name="ddlXQ" id="ddlXQ">
+	<option selected="selected" value=""></option>
+	<option value="2|春">春</option>
+	<option value="2|夏">夏</option>
+	<option value="2|短">短</option>
+	<option value="1|秋">秋</option>
+	<option value="1|冬">冬</option>
+	<option value="1|短">短</option>
+	<option value="1|暑">暑</option>
+
+</select>
+								成绩段：<SPAN style="BORDER-BOTTOM:2px inset; BORDER-LEFT:2px inset; BORDER-TOP:2px inset; BORDER-RIGHT:2px inset"><input name="txtQSCJ" type="text" maxlength="3" id="txtQSCJ" onkeydown="if (window.event.keyCode==13) window.event.keyCode=9;" style="TEXT-ALIGN:right" />-<input name="txtZZCJ" type="text" maxlength="3" id="txtZZCJ" /></SPAN></TD>
+						</TR>
+						<TR>
+							<TD>
+								<input type="submit" name="Button1" value="按学期查询" id="Button1" class="aspbutton" />
+								<input type="submit" name="Button5" value="按学年查询" id="Button5" class="aspbutton" />
+								<input type="submit" name="Button2" value="在校学习成绩查询" id="Button2" class="aspbutton" />
+								<input type="submit" name="Button6" value="学科讨论成绩查询" id="Button6" class="aspbutton" />
+								<input type="submit" name="Button7" value="通识教育实践成绩查询" id="Button7" class="aspbutton" />
+								<input type="submit" name="Button8" value="第二课堂及体测成绩查询" id="Button8" class="aspbutton" />
+								<input type="submit" name="Button9" value="过程成绩查询" id="Button9" class="aspbutton" />
+								<input type="submit" name="Button4" value="  打印  " id="Button4" class="aspbutton" onclick="window.print();" />
+								<input type="submit" name="Button3" value="  关闭  " id="Button3" class="aspbutton" onclick="window.close();" /></TD>
+							</TR>
+					</TABLE>
+					<BR>
+					<TABLE id="Table1" cellSpacing="0" cellPadding="3" width="100%">
+						<TR>
+							<TD align="center" colspan="3"><span id="Label4"><b><font size="4">2016-2017学年学习成绩</font></b></span></TD>
+						</TR>
+						<TR>
+							<TD><span id="Label3">学号：</span></TD>
+							<TD><span id="Label5">姓名：</span></TD>
+							<TD><span id="Label6">学院：</span></TD>
+						</TR>
+						<TR>
+							<TD colSpan="2"><span id="Label9">类(专业)：</span></TD>
+							<TD><span id="Label8">行政班：</span></TD>
+						</TR>
+					</TABLE>
+					<table cellspacing="0" cellpadding="3" rules="all" bordercolor="#B0B0B0" border="1" id="DataGrid1" width="100%">
+	<tr class="datagridhead">
+		<td>选课课号</td><td>课程名称</td><td>成绩</td><td>学分</td><td>绩点</td><td>补考成绩</td>
+	</tr><tr>
+		<td>(2016-2017-1)-021E0010-0092466-5</td><td>思想道德修养与法律基础</td><td>83</td><td>2.5</td><td>3.90</td><td>&nbsp;</td>
+	</tr><tr class="datagrid1212">
+		<td>(2016-2017-1)-021E0020-0015148-2</td><td>中国近现代史纲要</td><td>85</td><td>2.5</td><td>3.90</td><td>&nbsp;</td>
+	</tr><tr>
+		<td>(2016-2017-1)-021L0020-0091135-1</td><td>法学基础</td><td>89</td><td>1.5</td><td>4.50</td><td>&nbsp;</td>
+	</tr><tr class="datagrid1212">
+		<td>(2016-2017-1)-051F0020-0097391-2</td><td>大学英语Ⅲ</td><td>80</td><td>3.0</td><td>3.60</td><td>&nbsp;</td>
+	</tr><tr>
+		<td>(2016-2017-1)-081C0130-0088095-2</td><td>工程图学</td><td>84</td><td>2.5</td><td>3.90</td><td>&nbsp;</td>
+	</tr><tr class="datagrid1212">
+		<td>(2016-2017-1)-101M0010-0097020-2</td><td>电工电子工程训练</td><td>84</td><td>1.5</td><td>3.90</td><td>&nbsp;</td>
+	</tr><tr>
+		<td>(2016-2017-1)-101M0030-0092421-1</td><td>机电一体化基础</td><td>90</td><td>1.5</td><td>4.50</td><td>&nbsp;</td>
+	</tr><tr class="datagrid1212">
+		<td>(2016-2017-1)-211G0230-0098247-2</td><td>计算机科学基础</td><td>85</td><td>2.0</td><td>3.90</td><td>&nbsp;</td>
+	</tr><tr>
+		<td>(2016-2017-1)-40100800-0079227-2</td><td>游泳(初级班）</td><td>80</td><td>1.0</td><td>3.60</td><td>&nbsp;</td>
+	</tr><tr class="datagrid1212">
+		<td>(2016-2017-1)-821T0010-0094311-1</td><td>微积分（甲）Ⅰ</td><td>81</td><td>4.5</td><td>3.60</td><td>&nbsp;</td>
+	</tr><tr>
+		<td>(2016-2017-1)-821T0050-0092478-1</td><td>线性代数（甲）</td><td>72</td><td>2.5</td><td>2.70</td><td>&nbsp;</td>
+	</tr><tr class="datagrid1212">
+		<td>(2016-2017-2)-03110021-0016523-1</td><td>军训</td><td>81</td><td>2.0</td><td>3.60</td><td>&nbsp;</td>
+	</tr><tr>
+		<td>(2016-2017-2)-031E0010-0089257-3</td><td>军事理论</td><td>77</td><td>1.5</td><td>3.30</td><td>&nbsp;</td>
+	</tr><tr class="datagrid1212">
+		<td>(2016-2017-2)-051F0030-0097391-1</td><td>大学英语Ⅳ</td><td>76</td><td>3.0</td><td>3</td><td>&nbsp;</td>
+	</tr><tr>
+		<td>(2016-2017-2)-061B0010-0012125-1</td><td>常微分方程</td><td>77</td><td>1.0</td><td>3.30</td><td>&nbsp;</td>
+	</tr><tr class="datagrid1212">
+		<td>(2016-2017-2)-081C0251-0088103-2</td><td>工程训练</td><td>83</td><td>1.5</td><td>3.90</td><td>&nbsp;</td>
+	</tr><tr>
+		<td>(2016-2017-2)-211G0210-0098253-1</td><td>C程序设计</td><td>85</td><td>3.0</td><td>3.90</td><td>&nbsp;</td>
+	</tr><tr class="datagrid1212">
+		<td>(2016-2017-2)-241S0100-0004147-1</td><td>中国土地制度问题概论</td><td>C+</td><td>3.0</td><td>2.80</td><td>&nbsp;</td>
+	</tr><tr>
+		<td>(2016-2017-2)-371E0010-0006084-2</td><td>形势与政策Ⅰ</td><td>69</td><td>1.0</td><td>2.40</td><td>&nbsp;</td>
+	</tr><tr class="datagrid1212">
+		<td>(2016-2017-2)-40100801-0013023-1</td><td>游泳（中级班）</td><td>88</td><td>1.0</td><td>4.20</td><td>&nbsp;</td>
+	</tr><tr>
+		<td>(2016-2017-2)-691B0010-0010035-1</td><td>海洋概论</td><td>73</td><td>3.0</td><td>2.70</td><td>&nbsp;</td>
+	</tr><tr class="datagrid1212">
+		<td>(2016-2017-2)-761T0010-0094169-2</td><td>大学物理（甲）Ⅰ</td><td>56</td><td>4.0</td><td>0</td><td>&nbsp;</td>
+	</tr><tr>
+		<td>(2016-2017-2)-821T0020-0087039-1</td><td>微积分（甲）Ⅱ</td><td>52</td><td>3.5</td><td>0</td><td>&nbsp;</td>
+	</tr>
+</table>
+
+
+					<TABLE width="100%">
+						<TR>
+							<TD align="right" colSpan="4">
+								<img id="Image1" src="" alt="学生条形码" border="0" /></TD>
+						</TR>
+					</TABLE>
+				</div>
+			</div>
+			<div class="indexbot">浙江大学本科生院 版权所有</div>
+		</form>
+	</body>
+</HTML>
+
+"##;
