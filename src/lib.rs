95beec172283379e8caebb4b4a8500d787405ba9
Hexilee <hexileee@gmail.com>
2019-08-09 19:40:28 +0800

    complete req::LoginBody

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,20 @@
+#![feature(custom_attribute, async_await, param_attrs)]
+#![allow(unused_attributes)]
+
 pub mod req;
 pub mod resp;
+
+use interfacer_http::{content_types, http::header::COOKIE, http_interface, Response, Result};
+
+#[http_interface]
+trait JWInterface: Clone {
+    //    #[put("/api/user/{id}?age={age}")]
+    //    #[expect(200, content_types::APPLICATION_JSON)]
+    //    async fn put_user(
+    //        &self,
+    //        id: u64,
+    //        age: i32,
+    //        #[body] user: &User,
+    //        #[header(COOKIE)] cookie: &str
+    //    ) -> Result<Response<User>>;
+}
--- src/req.rs
+++ src/req.rs
@@ -1,1 +1,40 @@
+use interfacer_http::derive::ToContent;
+use serde_derive::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, ToContent, Debug)]
+pub struct LoginBody<'a> {
+    #[serde(rename = "__EVENTTARGET")]
+    event_target: &'a str,
+
+    #[serde(rename = "__EVENTARGUMENT")]
+    event_argument: &'a str,
+
+    #[serde(rename = "__VIEWSTATE")]
+    view_state: &'a str,
+
+    #[serde(rename = "TextBox1")]
+    username: &'a str,
+
+    #[serde(rename = "TextBox2")]
+    password: &'a str,
+
+    #[serde(rename = "RadioButtonList1")]
+    radio_button: &'a str,
+
+    #[serde(rename = "Text1")]
+    text1: &'a str,
+}
+
+impl<'a> LoginBody<'a> {
+    pub fn new(view_state: &'a str, username: &'a str, password: &'a str) -> Self {
+        Self {
+            event_target: "Button1",
+            event_argument: "",
+            view_state,
+            username,
+            password,
+            radio_button: "学生",
+            text1: "",
+        }
+    }
+}
