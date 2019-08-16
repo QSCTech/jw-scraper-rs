c4da370c452c148e37ed4df2b42f71c89828bd60
Hexilee <hexileee@gmail.com>
2019-08-16 14:29:12 +0800

    get_login_page 403

---
+++ interface-test/jw.rs
@@ -0,0 +1,34 @@
+#![feature(async_await)]
+
+use config::ConfigError;
+use interfacer_http::HttpService;
+use interfacer_http_hyper::{Service, Client};
+use serde::{Deserialize, Serialize};
+use tokio::prelude::*;
+use zju_jw_scraper::JWInterface;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    pub stu_id: String,
+    pub password: String,
+    pub jwb_base_url: String,
+}
+
+impl Config {
+    pub fn parse() -> Result<Self, ConfigError> {
+        let mut settings = config::Config::default();
+        settings
+            .merge(config::File::with_name("test-settings").required(false))?
+            .merge(config::Environment::with_prefix("TEST"))?;
+        settings.try_into::<Self>()
+    }
+}
+
+#[tokio::test]
+async fn test_login() -> Result<(), failure::Error> {
+    let config = Config::parse()?;
+    let service = Service::new(config.jwb_base_url.parse()?);
+    let login_page = service.get_login_page().await?;
+    Ok(())
+}
+
