470c0be2f616d2c0e31b98a9df908b52943b3d35
Hexilee <hexileee@gmail.com>
2019-08-25 16:52:43 +0800

    complete JWService::login

--- src/service.rs
+++ src/service.rs
@@ -6,7 +6,12 @@ use crate::raw::{RawJWService, JWB_COOKIE_NAME};
 use crate::{
     Course, CourseInfo, CourseSemester, Exam, ExamSemester, MajorScore, SchoolYear, Score,
 };
-use interfacer_http::{async_trait, cookie::Cookie, HttpClient, ResponseExt};
+use interfacer_http::{
+    async_trait,
+    http::header::SET_COOKIE,
+    http::{response::Parts, Response},
+    HttpClient, ResponseExt, Unexpected,
+};
 
 #[async_trait]
 pub trait JWService {
@@ -46,7 +51,26 @@ where
     async fn login(&self, stu_id: &str, password: &str) -> Result<String, Self::Err> {
         let resp =
             RawJWService::login(self, LoginBody::new(LOGIN_VIEW_STATE, stu_id, password)).await?;
-        unimplemented!()
+        let cookies = resp.cookie_map().map_err(|err| {
+            let mut copied_resp = Response::new(Vec::new());
+            *copied_resp.status_mut() = resp.status();
+            *copied_resp.version_mut() = resp.version();
+            *copied_resp.headers_mut() = resp.headers().clone();
+            Unexpected::new((SET_COOKIE, err.to_string()).into(), copied_resp)
+        })?;
+        let result = cookies.get(JWB_COOKIE_NAME);
+        match result {
+            Some(cookies) if cookies.len() > 0 => Ok(cookies[0].to_string()),
+            _ => Err(Unexpected::new(
+                (
+                    SET_COOKIE,
+                    format!("cookie named '{}' not found", JWB_COOKIE_NAME),
+                )
+                    .into(),
+                Response::from_parts(resp.into_parts().0, Vec::new()),
+            )
+            .into()),
+        }
     }
     async fn get_course_info(&self, code: &str) -> Result<CourseInfo, Self::Err> {
         unimplemented!()
