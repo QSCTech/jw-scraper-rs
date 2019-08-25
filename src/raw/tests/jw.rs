2815cbd8ae1d20ef5d518616dedec138c95aa38d
Hexilee <hexileee@gmail.com>
2019-08-25 11:25:17 +0800

    pass test_scores_base

--- src/raw/tests/jw.rs
+++ src/raw/tests/jw.rs
@@ -1,6 +1,6 @@
 use crate::raw::{JWService, JWB_COOKIE_NAME};
-use crate::raw::resp::{LoginPage, HiddenForm, CoursesPage, ExamsPage};
-use crate::raw::req::{LoginBody, CoursesReq, ExamsReq, LOGIN_VIEW_STATE, DEFAULT_COURSES_VIEW_STATE, DEFAULT_EXAMS_VIEW_STATE};
+use crate::raw::resp::{LoginPage, HiddenForm, CoursesPage, ExamsPage, ScoresBasePage, ScoresPage};
+use crate::raw::req::{LoginBody, CoursesReq, ExamsReq, ScoresReq, LOGIN_VIEW_STATE, DEFAULT_COURSES_VIEW_STATE, DEFAULT_EXAMS_VIEW_STATE, SCORES_BASE_VIEW_STATE};
 use config::ConfigError;
 use interfacer_http::{Helper, http::Response, ResponseExt, cookie::Cookie};
 use interfacer_http_hyper::Client;
@@ -177,5 +177,31 @@ async fn test_exams() -> Result<(), Box<dyn std::error::Error>> {
         cookie_str,
     ).await?.into_body();
     assert!(exams.exams.len() > 0);
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_scores_base() -> Result<(), Box<dyn std::error::Error>> {
+    let config = Config::parse()?;
+    let service = Client::new().with_helper(
+        Helper::new()
+            .with_base_url(config.jwb_base_url.parse()?)
+            .with_request_initializer(crate::helper::request_initializer)
+    );
+    let resp: Response<()> = service.login(
+        LoginBody::new(
+            LOGIN_VIEW_STATE,
+            config.stu_id.as_str(),
+            config.password.as_str(),
+        )
+    ).await?;
+    let cookies = resp.cookie_map()?;
+    let cookie = cookies.get(JWB_COOKIE_NAME);
+    assert!(cookie.is_some());
+    assert_eq!(1, cookie.unwrap().len());
+    let (name, value) = cookie.unwrap()[0].name_value();
+    let cookie_str = Cookie::new(name.to_owned(), value.to_owned()).to_string();
+    let scores_base: ScoresBasePage = service.get_scores_base(&config.stu_id, cookie_str).await?.into_body();
+    assert_eq!(SCORES_BASE_VIEW_STATE, &scores_base.hidden_form.view_state);
     Ok(())
 }
